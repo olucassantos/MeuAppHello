b0f19c8a2e473cf0a29c39a6a06596e256666b1d
Lucas Anjos <oargus.g@gmail.com>
2024-04-23 09:24:42 -0300

    Adicionar componente MostraBemVindo ao App.js

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MostraBemVindo from './components/MostraBemVindo';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <MostraBemVindo />
+
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/MostraBemVindo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from "react-native";
+
+const MostraBemVindo = () => {
+    return <Text>Bem vindo ao Hello!</Text>
+}
+
+export default MostraBemVindo;
